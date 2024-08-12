f8f1415a7cdcca514e0cf00fe91c1c79766d6acf
Allison <allison.gretsinger@gmail.com>
2024-08-12 09:52:56 -0400

    main.py now prints word total at end

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
     print(file_contents)
+    words = file_contents.split()
+    word_total = len(words)
+    print(f"This book contains a total of {word_total} words.")
 
 main()
