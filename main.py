a7fa81d2c6e5bde19102d9ff9f24071527abe376
Allison <allison.gretsinger@gmail.com>
2024-08-12 12:37:01 -0400

    it sorts counted letters now!

--- main.py
+++ main.py
@@ -2,24 +2,45 @@ def main():
     book_path = "books/frankenstein.txt" # will allow custom inputs later
     book_text = read_text(book_path)
     word_count = count_words(book_text)
-    print(f"This book contains {word_count} words. What follows is a count of each letter:")
-    print(count_each_letter(book_text))
+    print("========== Allie's Book Reporrrrrrrrt! ==========")
+    print("Dum, tss, dum dum dum, tss... dum dum dum. dum. dum dum dum")
+    print("Allie's book report!")
+    print(f"This book contains {word_count} words.", end="")
+    letter_count = count_each_letter(book_text)
+    sorted_count = sort_letters(letter_count)
+    for entry in sorted_count:
+        print(f"\nThe letter {entry} appears {sorted_count[entry]} times.", end=" ")
+    print("so few! very smol number ^.^")
+    print("Thank you for reading Allie's book report!")
+
+def sort_letters(dict):
+    dict_list = []
+    for key in dict:
+        dict_list.append({key: dict[key]})
+    dict_list.sort(reverse=True, key=sort_on)
+    sorted_dict = {}
+    for entry in dict_list:
+        sorted_dict.update(entry)
+    return sorted_dict
+
+def sort_on(dict):
+    return list(dict.values())[0]
 
 def count_each_letter(text):
     letter_count = {}
     letter_count = init_alphabetized_letters(letter_count)
     lowered_text = text.lower()
     for letter in lowered_text:
-        if letter in "qwertyuiopasdfghjklzxcvbnm": # make sure the character is an actual letter
+        if letter.isalpha() == True: # make sure the character is an actual letter
             letter_count[letter] += 1
     return letter_count
 
-# start by putting the keys in alphabetical order because we're tidy
-def init_alphabetized_letters(dic):
+# initialize the keys in alphabetical order because we're tidy
+def init_alphabetized_letters(dict):
     lower_alpha = "abcdefghijklmnopqrstuvwxyz"
     for letter in lower_alpha:
-        dic[letter] = 0
-    return dic
+        dict[letter] = 0
+    return dict
     
 def count_words(text):
     return len(text.split())
