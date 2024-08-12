c7c9f8e3b0fc3fdf7ce3c8cf11edb89fa951dfcc
Allison <allison.gretsinger@gmail.com>
2024-08-12 10:12:11 -0400

    split main() into some more functions

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-    print(file_contents)
-    words = file_contents.split()
-    word_total = len(words)
-    print(f"This book contains a total of {word_total} words.")
+    book_path = "books/frankenstein.txt"
+    book_text = read_text(book_path)
+    word_count = count_words(book_text)
+    print(f"This book contains {word_count} words.")
+
+def count_words(text):
+    return len(text.split())
+
+def read_text(path):
+    with open(path) as f:
+        return f.read()
 
 main()
